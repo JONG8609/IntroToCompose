3cc22486725610fce616be3f0235d28025f1314e
jongjin <jjoh@otosone.com>
2023-08-11 16:46:38 +0900

    first

---
+++ app/src/main/java/com/example/introtocompose/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.introtocompose
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.introtocompose.ui.theme.IntroToComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IntroToComposeTheme {
+                // A surface container using the 'background' color from the theme
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        color = Color(0xFF546E7A)
+    ) {
+        CreateCircle()
+    }
+}
+@Preview
+@Composable
+fun CreateCircle() {
+    Card(modifier = Modifier
+        .padding(3.dp)
+        .size(105.dp)
+        .clickable {
+            Log.d("Tap", "CreateCircle: Tap")
+        },
+        shape = CircleShape,
+        elevation = 4.dp) {
+        Box(contentAlignment = Alignment.Center){
+            Text(text = "Tap", modifier = Modifier)
+        }
+
+
+    }
+}
+
+
+@Preview(name = "Me", showBackground = true)
+@Composable
+fun DefaultPreview() {
+    IntroToComposeTheme {
+        MyApp()
+    }
+}
